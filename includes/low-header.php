a429bdb4fdc6d431ea9a084e5213c79cb9d94aa3
endritthaqi <119794910+endritthaqi@users.noreply.github.com>
2023-02-26 18:50:10 +0100

    responsive finished [MAIN PAGES without dashboard]

--- includes/low-header.php
+++ includes/low-header.php
@@ -7,16 +7,18 @@
             ><img src="/img/bx-menu.svg" alt="MENU" style="height: 30px"
           /></a>
         </div>
-
+        
         <?php 
         if (isset($_SESSION['role'])) {
           ?>
-          <a href="dashboard/">
-            <img src="dashboard/<?php echo $_SESSION['profile'] ?>" alt="Placeholder Image" style="height: 50px;width: 50px;border-radius: 50%;overflow: hidden" />
-          </a>
-          <a href="/logout.php" style="color:black">
-                <i class="bx bx-log-out"></i> Logout
-            </a>
+          <div class="fotoLogOut">
+                <a href="dashboard/">
+                  <img src="dashboard/<?php echo $_SESSION['profile'] ?>" alt="Placeholder Image" style="height: 50px;width: 50px;border-radius: 50%;overflow: hidden" />
+                </a>
+                <a href="/logout.php" style="color:black">
+                      <i class="bx bx-log-out"></i> Logout
+                </a>
+            </div>
           <?php
         }
         else{
